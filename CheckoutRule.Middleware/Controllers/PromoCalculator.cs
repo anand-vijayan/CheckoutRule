7600f511b5c5fcdcba49eb01ba9c5c0e772b76e0
Anand Vijayan <anand.vijayan@outlook.com>
2021-09-23 01:24:28 +0530

    Initial checkin of calculation logic
    Created main method for the calculation logic

--- CheckoutRule.Middleware/Controllers/PromoCalculator.cs
+++ CheckoutRule.Middleware/Controllers/PromoCalculator.cs
@@ -7,13 +7,38 @@ namespace CheckoutRule.Middleware.Controllers
 {
     public class PromoCalculator : IPromoCalculator
     {
+        #region Private Methods
+
         private bool IsListObjectValid<T>(List<T> tObject) => (tObject != null && tObject.Count > 0);
 
+        private void ApplyBundlePromo(List<Item> items, List<Promo> promos, Cart cart)
+        {
+            throw new NotImplementedException();
+        }
 
+        private void ApplyComboPromo(List<Item> items, List<Promo> promos, Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
 
         public Cart ApplyPromosInCart(List<Item> items, List<Promo> promos, Cart cart)
         {
-            throw new NotImplementedException();
+            if (cart != null
+                && IsListObjectValid(cart.Items)
+                && IsListObjectValid(items)
+                && IsListObjectValid(promos))
+            {
+                ApplyBundlePromo(items, promos, cart);
+                ApplyComboPromo(items, promos, cart);
+            }
+            else
+            {
+                Console.WriteLine("ApplyPromosInCart: Input not valid!!!");
+            }
+
+            return cart;
         }
     }
 }
